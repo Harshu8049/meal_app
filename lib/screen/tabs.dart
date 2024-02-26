4eeae12a9da5fbe2d1d998a9f7da02c7b59d91e7
Harshu8049 <harshsharma8049@gmail.com>
2024-02-26 11:43:16 +0530

    show message when we add or remove the favourite

--- lib/screen/tabs.dart
+++ lib/screen/tabs.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
 import 'package:meal_app/screen/categories.dart';
 import 'package:meal_app/screen/meals.dart';
+import 'package:meal_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -13,6 +15,33 @@ class TabsScreen extends StatefulWidget {
 
 class _TabsScreenState extends State<TabsScreen> {
   int _slectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 4),
+        content: Text(message),
+      ),
+    );
+  }
+
+  void _toggleMealFavoriteStatus(Meal meal) {
+    final isExisting = _favoriteMeals.contains(meal);
+
+    if (isExisting == true) {
+      setState(() {
+        _favoriteMeals.remove(meal);
+        _showInfoMessage('Meal is no Longer a Favorite.');
+      });
+    } else {
+      setState(() {
+        _favoriteMeals.add(meal);
+        _showInfoMessage('Marked as a favorite!');
+      });
+    }
+  }
+
   void _selectPage(int index) {
     setState(() {
       _slectedPageIndex = index;
@@ -21,14 +50,22 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    Widget activePage = const CategoriesScreen();
+    Widget activePage = CategoriesScreen(
+      onToogleFavorite: _toggleMealFavoriteStatus,
+    );
     var activePageTitle = 'Categories';
     if (_slectedPageIndex == 1) {
-      activePage = const MealScreen(meals: []);
+      activePage = MealScreen(
+        meals: _favoriteMeals,
+        onToggleFavorite: _toggleMealFavoriteStatus,
+      );
       activePageTitle = 'Your Favorites';
     }
     return Scaffold(
-        appBar: AppBar(title: Text(activePageTitle)),
+        appBar: AppBar(
+          title: Text(activePageTitle),
+        ),
+        drawer: const MainDrawer(),
         body: activePage,
         bottomNavigationBar: BottomNavigationBar(
           onTap: _selectPage,
